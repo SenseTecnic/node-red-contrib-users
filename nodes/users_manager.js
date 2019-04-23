018e3295e188fffe53b72bbf48d3e6b3e7f57334
Linux User <aarcov@gmail.com>
2019-04-23 21:13:13 +0000

    Squashed commit of the following:
    commit 1d3ef52302fd5603b235ac8c8898e28efb9ca231
    Author: Linux User <aarcov@gmail.com>
    Date:   Tue Apr 23 21:10:26 2019 +0000
    
        Defining procedure for handling messages
        The locales/en-US/users_manager.json may not be included properly
    
    commit 601dc5c12d086bc1f30582fb87c5f897efd0d378
    Author: Linux User <aarcov@gmail.com>
    Date:   Tue Apr 23 19:51:18 2019 +0000
    
        Connecting the User Credential Manager Node
        Connected the User Credential Manager Node to access the user account
        objects.  Do note that the passwords are stored as a hash, so they are
        not able to be displayed; however, this gives us the access we need to
        enable overwriting of the passwords and the creation of new accounts.
    
    commit ee377f62ad0f83ffeb25bf925dd52f1910d424e5
    Author: Linux User <aarcov@gmail.com>
    Date:   Tue Apr 23 17:06:15 2019 +0000
    
        Starting on making custom node
        Need to add in error handling for messages (see section at the
        beginning, remember to use locals for language support)
        Need to add in logic, identify functions to use

--- nodes/users_manager.js
+++ nodes/users_manager.js
@@ -1,11 +1,108 @@
-module.exports = function(RED) {
-    function UsersManagerNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
-        });
-    }
-    RED.nodes.registerType("users_manager",UsersManagerNode);
+var path = require('path');
+var users = require('../users');
+
+
+function checkedRequiredFields(RED, node, config, msg) {
+  // Message testing conditions
+  /*
+  if !(msg.hasOwnProperty('user_management')){
+    throw new Error("users.errors.user-management-required");
+  }
+  if !(msg.user_management.hasOwnProperty('action')){
+    throw new Error("users.errors.user-management-action-required");
+  }
+  if !(msg.user_management.hasOwnProperty('username')){
+    throw new Error("users.errors.user-management-username-required");
+  }
+  if !(msg.user_management.hasOwnProperty('password')){
+    throw new Error("users.errors.user-management-password-required");
+  }
+  */
 }
+
+module.exports = function (RED) {
+
+  function UsersManagerNode(n) {
+    RED.nodes.createNode(this,n);
+    var node = this;
+    node.status({});
+
+    // Searching though all nodes to find the users_config node
+    // to extract the credentials from
+    var config;
+    RED.nodes.eachNode(function (n) {
+      if (n.type === "users_config") {
+        config = n;
+        config.credentials = RED.nodes.getCredentials(n.id);
+      }
+    });
+
+    node.on('input', function (msg) {
+      try {
+        checkedRequiredFields(RED, node, config, msg);
+      } catch (err) {
+        node.error(RED._(err.message));
+        node.status({fill: "red", shape: "ring", text: RED._(err.message)});
+        msg.res.status(500);
+        msg.res.send("Error: invalid config");
+      }
+
+      user_m = msg.user_management;
+      switch(user_m.action) {
+        case 'create':
+	  if(users.getUser('operator','operator') == null){
+	    // Create the new user
+            user_m.result = "Creation request";
+	  }
+	  else{
+	    // We have encountered an error
+            user_m.error = "Creation request error";
+	  }
+	  break;
+	case 'update':
+	  if(users.getUser('operator','operator') != null){
+	    // Update the user
+            user_m.result = "Update request"
+	  }
+	  else{
+	    // We have encountered an error
+            user_m.error = "Update request error";
+	  }
+	  break;
+	case 'update_or_create':
+          // Create the user (we can be indiscriminate b/c
+          // we want to overwrite the old record if it exists
+          user_m.result = "Update or Create request";
+	  break;
+	case 'delete': 
+	  if(users.getUser('operator','operator') != null){
+	    // Delete the user
+            user_m.result = "Delete request";
+	  }
+	  else{
+	    // We have encountered an error
+            user_m.error = "Delete request error";
+	  }
+	  break;
+	default:
+	  // Default action
+          user_m.error = "Default action triggered";
+      }
+
+      if (user_m.hasOwnProperty('error')) {
+        node.status({fill: "yellow", shape: "dot", text: "Invalid"});
+        //node.send([msg, null]);
+      } else {
+        node.status({fill: "green", shape: "dot", text: "Valid"});
+      }
+
+      // Testing function
+      msg.user_management = user_m;
+      credentials = config.credentials;
+      msg.payload = credentials;
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("users_manager", UsersManagerNode);
+};
