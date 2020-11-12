f0dd3a8d233cb7cb4505166c9d43532a6472b6f2
Alberto Schena <alberto.schena@gizeroenergie.it>
2020-11-12 18:05:40 +0100

    Max-Age converted to duration format

--- nodes/users_config.js
+++ nodes/users_config.js
@@ -1,6 +1,38 @@
 var users = require('../users');
 
-var DEFAULT_JWT_COOKIE_MAX_AGE = 604800000; // 7 days
+function parseDuration(string)
+{
+  const SECONDS = 1000;
+  const MINUTES = 60 * SECONDS;
+  const HOURS = 60 * MINUTES;
+  const DAYS = 24 * HOURS;
+  const WEEKS = 7 * DAYS;
+  const MONTHS = 30 * DAYS;
+  const YEARS = 365 * DAYS;
+
+  function intGroup1(match)
+  {
+    return match && parseInt(match[1]);
+  }
+
+  function captureField(string, unit)
+  {
+    return string && intGroup1(string.match(new RegExp(`\\b(\\d+)${unit}\\b`))) || 0;
+  }
+
+  return (
+    captureField(string, 'ms') +
+    captureField(string, 's') * SECONDS +
+    captureField(string, 'm') * MINUTES +
+    captureField(string, 'h') * HOURS +
+    captureField(string, 'd') * DAYS +
+    captureField(string, 'w') * WEEKS +
+    captureField(string, 'M') * MONTHS +
+    captureField(string, 'y') * YEARS
+  );
+}
+
+var DEFAULT_JWT_COOKIE_MAX_AGE = parseDuration('1w');
 var DEFAULT_APP_PATH = '/users';
 
 module.exports = function (RED) {
@@ -30,14 +62,9 @@ module.exports = function (RED) {
       return;
     }
 
-    // if (!n.jwtCookieMaxAge) {
-    //   node.error(RED._("users.errors.missing-jwt-cookie-max-age"));
-    //   return;
-    // }
-
     node.credentials = credentials;
     node.jwtCookieName = n.jwtCookieName;
-    node.jwtCookieMaxAge = n.jwtCookieMaxAge || DEFAULT_JWT_COOKIE_MAX_AGE;
+    node.jwtCookieMaxAge = parseDuration(n.jwtCookieMaxAge) || DEFAULT_JWT_COOKIE_MAX_AGE;
     node.jwtHttpsOnly = n.jwtHttpsOnly === true;
     node.appPath = n.appPath || DEFAULT_APP_PATH;
     users.init(RED, node);
